905af1c7cdf3b335f1ef181d6be33107bc14f071
Jian Jin <jijin@ucdavis.edu>
2016-02-07 10:08:06 -0800

    color picker prototype

--- playGround/color_gadget.R
+++ playGround/color_gadget.R
@@ -1,46 +1,45 @@
 # http://shiny.rstudio.com/articles/gadget-ui.html
+# addin: https://rstudio.github.io/rstudioaddins/
+# add insert color
+# modulize, clean up
+# select pallete
+# create a function, display color
+
 library(shiny)
 library(miniUI)
+library(colorspace)
 
-myColorFunc <- function() {
+colorPicker <- function() {
   
   ui <- miniPage(
-    gadgetTitleBar("Displaying Colors"),
-    fillCol(flex = c(2, 2, 2, 6),
-            div(sliderInput("alpha", "alpha", min = 0, max = 1, value = 0.75), 
-                align = "center"),
-            div(sliderInput("n", label = "number of colors", min = 7, max = 24, value = 12), 
-                align = "center"),
-            plotOutput("plot", click = "plot_click"),
-            plotOutput("plot_mainColor")
-            
+    gadgetTitleBar("Color Picker"),
+    fillCol(flex = c(NA, 1),
+            div(plotOutput("plot", 
+                           click = "plot_click", 
+                           height = "20px", width = "400px"),
+                        align = 'center'),
+            div(plotOutput("plot2", click = "plot2_click",
+                          height = "250px", width = "320px"),
+                align = 'center')
     )
-    # miniContentPanel(
-      # Define layout, inputs, outputs
-    #   sidebarPanel(
-    #     sliderInput("color", "value", 1, 12, 1),
-    #     sliderInput("alpha", "alpha", 0, 1, 0.05)
-    #   ),
-    #   mainPanel(
-    #    plotOutput("plot")
-    #  )
-    # )
   )
   
   server <- function(input, output, session) {
     # Define reactive expressions, outputs, etc.
     output$plot <- renderPlot({
-      plotRainBow(input$n, input$alpha, axes = F)
+      plotRainBow(24, 0.95, axes = F)
     })
+    
     main_colors <- reactive({
       col_index <- floor(input$plot_click[[1]]/2) + 1
-      main_col_hex <- colorspace::rainbow_hcl(12)[col_index]
+      main_col_hex <- rainbow_hcl(24)[col_index]
       main_col_rgb <- col2rgb(main_col_hex) / 255
       rgb_matrix <- powerful_color(main_col_rgb)
     })
     
-    output$plot_mainColor <- renderPlot({
-      plot(c(0, 25), c(0, 24), type = "n", xlab = "", ylab = "", bty = 'n', axes = FALSE)
+    output$plot2 <- renderPlot({
+      op <- par(mar = c(0, 0, 0, 0))
+      plot(c(0, 32), c(0, 24), type = "n", xlab = "", ylab = "", bty = 'n', axes = TRUE)
       alpha_values = seq(0.1, 1, length.out = 12)
       rvalues = main_colors()[1, ]
       gvalues = main_colors()[2, ]
@@ -50,6 +49,12 @@ myColorFunc <- function() {
                    red = rvalues, green = gvalues, blue = bvalues,
                    alpha = alpha_values[i])
       }
+      # box(lty = '1373', col = 'red')
+      par(op)
+    })
+    
+    output$color_info <- renderText({
+      str(input$plot2_click)
     })
     
     # When the Done button is clicked, return a value
@@ -65,8 +70,8 @@ myColorFunc <- function() {
 
 plotRainBow <- function(n = 12, alpha, axes = TRUE) {
   i = 1:n
-  op <- par(mar = c(0.5, 0.5, 0.5, 0.5))
-  plot(c(0, 2 * n), c(0, 2), type = "n", xlab = "", ylab = "", bty = 'n', axes = axes)
+  op <- par(mar = c(0, 0, 0, 0))
+  plot(c(0, 2 * n), c(0, 2), type = "n", xlab = "", ylab = "", bty = "n", axes = axes)
   rect(xleft = 0 + 2 * i - 2, ybottom = 0, xright = 2 * i, ytop = 2, 
        col = rainbow_hcl(n, alpha = alpha), 
        border = FALSE)
@@ -74,12 +79,21 @@ plotRainBow <- function(n = 12, alpha, axes = TRUE) {
 }
 
 powerful_color <- function(x){
-  cbind(x ^ 0.2, x ^ 0.3, x ^ 0.5, x ^ 0.6, x ^ 0.8, x, x ^ 1.5, x ^ 2, x ^ 3, x ^ 4, x ^ 5, x^ 6)
+  x <- abs(x - 0.01)
+  cbind(x ^ 0.05, x ^ 0.1, x ^ 0.2, x ^ 0.3, 
+        x ^ 0.4, x ^ 0.6, x ^ 0.8, x ^ 1, 
+        x ^ 1.5, x ^ 2, x ^ 3, x ^ 4, 
+        x ^ 5, x ^ 6.5, x ^ 8, x ^ 10)
 }
 
 plot_a_row <- function(ybottom, ytop, red, green, blue, alpha = 1){
-  i = 1:12
+  i = 1:16
   rect(xleft = 0 + 2 * i - 2, ybottom, xright = 2 * i, ytop, 
        col = rgb(red, green, blue, alpha), 
        border = FALSE)
 }
+
+
+# ,
+# div(verbatimTextOutput("color_info"),
+#     align = 'center')
